0bb87439f23564ab78dae69255aa9e25a8e07fc1
Salman Mahmud <salman_mahmud@hotmail.com>
2022-11-20 20:05:07 -0500

    commit tree 686d8ab28e59176653169586885852ca90223916

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,15 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # find a cheese using the ID from the URL
+    # Send a JSON response using that cheese object
+
+    byebug
+
+    specificCheese = Cheese.find(params[:id])
+
+    render json: specificCheese
+  end
+
 end
